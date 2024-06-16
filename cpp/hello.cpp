1a4f58c624a7990b037206532f2e126f6bdcca44
Mazhar Bilgrami <70132390+Maz-Bil@users.noreply.github.com>
2024-06-16 14:59:01 -0700

    Update hello.cpp
    If you want to show students 'using namespace std' right off the cuff. I noticed myself and other tutors including it over the summer, only for students to ask what that is and how to use it.

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -5,3 +5,17 @@ int main()
   std::cout << "Hello World!" << std::endl;
 }
 
+/*
+This is the difference when using namespace std.
+By including "using namespace std;" we no longer need to prefix standard library names with "std::".
+
+With "using namespace std;":
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+}
+
+*/
